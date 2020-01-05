bd4ac2219b4c133921965107692293df1f9cd28a
aniaska4 <aniaska4@wp.pl>
2020-01-05 14:34:07 +0100

    create env, json and tepmates

---
+++ src/components/Slider.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+    .main
+        .title
+            | Hello Ania
+        button(@click="click")
+</template>
+<script>
+import loan from "@/assets/json/loan.json"
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        click() {
+            console.log(loan.data)
+        }
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
+

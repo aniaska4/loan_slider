12b1b6a4f624655ee3236ddea8c23d2f534c1d75
aniaska4 <aniaska4@wp.pl>
2020-01-05 20:42:20 +0100

    add styles to calculator

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -3,10 +3,10 @@
         .main_box
             .main_box__box_cell
                 .box_cell__box_title_main
-                    .box_cell__box_title_main--text Wybierz
+                    .box_cell__box_title_main--text Kalkulator raty pożyczki
                 .box_cell__box_valor
-                    .box_cell__box_valor--text Wartość pożyczki
-                        span.valor#amount  1200,00 zł
+                    .box_cell__box_valor--text Twoja kwota pożyczki:
+                        span.valor#amount  {{slider.value}} zł
                 .box_cell__box_section
                     .box_cell__box_section--minmax  600
                     .box_cell__box_section--minmax  3000
@@ -18,6 +18,8 @@
                           :processStyle="{ backgroundColor: '#ffffff6e' }"
                           :lineHeight="slider.lineHeight"
                           class="demo-demo" id="slider-1")
+                          template(slot="tooltip" style="background-color: #031d4c; border: black;" slot-scope="tooltip")
+                            img(src="/image/handle.png" style="background-color: white; margin-top: 10px; border-radius: 7px;")
                 .box_cell__box_title 
                     .parameters 
                         span#installment
@@ -27,6 +29,8 @@
                 .box_cell__title_parcela
                     #installment Valor de parcela: 
                         span.valor#rata 
+                .box_cell__button
+                    button weź pożyczkę
         .main
             .title
                 | Hello Ania
@@ -47,7 +51,7 @@ export default {
         return {
         slider: {
             lineHeight: 8,
-            value: 2000,
+            value: 300,
             data: [100, 200, 300, 400, 500]
             },
         }
@@ -62,14 +66,21 @@ export default {
 </script>
 
 <style scoped>
+.slider {
+  display: flex;
+  justify-content: center;
+  height: 450px;
+}
 .main_box {
   display: flex;
   display: -webkit-box;
   display: -moz-box;
   display: -ms-flexbox;
   display: -webkit-flex;
-  width: 38%;
-  background-color: #da2228;
+  width: 40%;
+  height: 100%;
+  background-color: #5e5e5f;
+  border: 2px solid rgb(0, 0, 27);
   color: white;
   border-radius: 10px;
   font-family: "Oswald", sans-serif;
@@ -97,7 +108,7 @@ export default {
   align-items: center;
 }
 .box_cell__box_section--minmax {
-  font-size: 14px;
+  font-size: 18px;
   line-height: 16px;
   margin: 0 20px 0 20px;
 }
@@ -125,6 +136,19 @@ export default {
   align-items: center;
   padding-bottom: 5px;
 }
+.box_cell__box_title_main{
+  margin-bottom: 20px;
+  font-size: 30px;
+  color: rgb(0, 0, 27);
+  font-weight: 600;
+  border-bottom: 1px solid rgb(0, 0, 27);
+  text-align: center;  
+  justify-content: center;
+  margin-bottom: 40px;
+}
+.box_cell__box_valor{
+    text-align: start;
+}
 .box_cell__box_section {
   display: flex;
   display: -webkit-box;
@@ -136,16 +160,42 @@ export default {
   align-items: center;
   margin-top: 10px;
 }
-.box_cell__box_slider {
+/* .box_cell__box_slider {
   padding: 30px 0 30px 0;
-}
+} */
 .box_cell__title_parcela {
   width: 50%;
   margin-top: 30px;
 }
+.box_cell__button button{
+	box-shadow: 0px 0px 0px 2px #9fb4f2;
+	background:linear-gradient(to bottom, #032257 5%, #476e9e 100%);
+	background-color:#032257;
+	border-radius:10px;
+	border:2px solid #4e6096;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:19px;
+	padding:12px 37px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #060f24;
+    width: 150px;
+    height: 60px;
+};
+.box_cell__button:hover{
+	background:linear-gradient(to bottom, #476e9e 5%, #032257 100%);
+	background-color:#476e9e;
+};
+.box_cell__button:active {
+	position:relative;
+	top:1px;
+};
 .valor {
   border-bottom: solid 1px white;
   font-weight: 500;
+  
 }
 .textInfo {
   width: 45%;
@@ -156,6 +206,9 @@ export default {
 }
 .clear {
   height: 50px;
+}
+.vue-slide-bar-tooltip{
+
 }
 
 </style>
